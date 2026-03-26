061fa528181422fa6a3d7b2a5e949907204a7882
Iniubong45 <inistephen44@gmail.com>
2026-03-26 17:34:03 +0100

    Javascript code for lab 5 has bee moved to an external file.

---
+++ lab5script.js
@@ -0,0 +1,18 @@
+let name = "Alice";
+console.log("Hello, " + name);
+let age = 18;
+console.log("You are " + age + " years old.");
+let major = "Computer Engineering";
+console.log("Your major is " + major);
+
+function greet(user) {alert("Welcome, " + user);}
+greet("Iniubong");
+
+function addition(a, b) {return a + b;}
+alert((addition(28, 12)));
+let num = parseFloat(prompt("Enter any number:"));
+if ((num % 2) === 0) {alert(num + " The number you entered is even.");
+
+} else {alert(num + " The number you entered is odd.");}
+let fruit_array = ["Water Melon", "Pineapple", "Mango", "Strawberry", "Qauva"];
+for (let i = 0; i < fruit_array.length; i++) {console.log(fruit_array[i]);}
